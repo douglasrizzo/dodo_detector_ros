7ca7192c804a7c00a714a48148caed1dd3dd66d9
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2018-05-20 19:52:01 -0300

    fixing message dependencies

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['dodo_detector_ros'],
     package_dir={'': 'src'},
+    dependency_links = ['https://github.com/douglasrizzo/dodo_detector']
 )
 
 setup(**setup_args)

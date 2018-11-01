1b17c8e92c2da6e6188482b153800da34224f729
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2018-11-01 16:38:43 -0300

    fixed frame is called global frame now

--- src/detector.py
+++ src/detector.py
@@ -24,9 +24,9 @@ class Detector:
         min_points = rospy.get_param('~sift_min_pts', 10)
         database_path = rospy.get_param('~sift_database_path', '')
         filters = rospy.get_param('~filters', {})
-        
-        self._fixed_frame = rospy.get_param('~fixed_frame', None)
-        
+
+        self._global_frame = rospy.get_param('~global_frame', None)
+
         # create a transform listener so we get the fixed frame the user wants
         # to publish object tfs related to
         self._tf_listener = tf.TransformListener()
@@ -131,8 +131,8 @@ class Detector:
 
                     # if the user passes a fixed frame, we'll ask for transformation
                     # vectors from the camera link to the fixed frame
-                    if self._fixed_frame is not None:
-                        (trans, _) = self._tf_listener.lookupTransform('/' + self._fixed_frame, '/camera_link', rospy.Time(0))
+                    if self._global_frame is not None:
+                        (trans, _) = self._tf_listener.lookupTransform('/' + self._global_frame, '/camera_link', rospy.Time(0))
 
                     # convert image from the subscriber into an OpenCV image
                     scene = self._bridge.imgmsg_to_cv2(self._current_image, 'rgb8')
@@ -200,14 +200,14 @@ class Detector:
                             if publish_tf:
                                 # kinect here is mapped as camera_link
                                 # object tf (x, y, z) must be
-                                # passed as (z,-x,-y)          
+                                # passed as (z,-x,-y)
                                 object_tf = [point_z, -point_x, -point_y]
                                 frame = 'camera_link'
 
                                 # translate the tf in regard to the fixed frame
-                                if self._fixed_frame is not None:                      
+                                if self._global_frame is not None:
                                     object_tf = numpy.array(trans) + object_tf
-                                    frame = self._fixed_frame
+                                    frame = self._global_frame
 
                                 self._tfpub.sendTransform(
                                     (object_tf),
